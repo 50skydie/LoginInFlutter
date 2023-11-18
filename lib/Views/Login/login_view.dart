c2cce0a1fccede6f4fbb87aa05c0769897fcb69e
Szymon Romanowski <36265196+50skydie@users.noreply.github.com>
2023-11-18 22:55:57 +0100

    added text and text inputs w/o styling

--- lib/Views/Login/login_view.dart
+++ lib/Views/Login/login_view.dart
@@ -17,21 +17,78 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Image.asset(MyImages.imageLogo),
-          ElevatedButton(
-            child: Text('Sign up',
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            //logo png
+            Image.asset(MyImages.imageLogo),
+
+            //Sign in simple text
+            Text('Sign in', textAlign: TextAlign.left,
                 style: TextStyle(
-                    color: MyColors.purpleColor)),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const RegisterView()),
-              );
-            },
-          ),
-        ],
+                    color: MyColors.purpleColor
+                )
+            ),
+
+            //User Name field
+            const TextField(
+              decoration: InputDecoration(
+                  hintText: 'Email or User Name',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.account_circle_outlined)
+              ),
+            ),
+
+            //Password field
+            const TextField(
+              decoration: InputDecoration(
+                hintText: 'Password',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.account_balance_wallet_outlined)
+              ),
+            ),
+
+            //Forget Password
+            Text('Forget Password ?', textAlign: TextAlign.right,
+                style: TextStyle(
+                    color: MyColors.purpleColor
+                )
+            ),
+
+            //Submit button
+            ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: MyColors.purpleColor,
+                  padding: EdgeInsets.symmetric(horizontal: 200, vertical: 20),
+                ),
+                child: Text(
+                    'Sign in', style: TextStyle(
+                    backgroundColor: MyColors.purpleColor
+                ),
+                ),
+            ),
+
+            //Or sign in With
+            const Text('Or sign in With',
+                style: TextStyle(
+                    color: Colors.deepPurple,
+                )
+            ),
+
+            // todo 4 icon logos
+
+            // dont have acc text?
+            const Text('Don\'t have account ? Sing Up',
+                style: TextStyle(
+                  color: Colors.deepPurple,
+                )
+            ),
+          ],
+        ),
       ),
     );
   }
